80236cb8cfa9b8bcaefd47bc9e7c37740ac3d4b4
wind8020 <nghti8020@gmail.com>
2023-07-21 18:01:08 +0700

    Update menu and slider

--- src/assets/Component/Content1.tsx
+++ src/assets/Component/Content1.tsx
@@ -1,20 +1,94 @@
+import React, { Component } from "react";
+import Slider from "react-slick";
+import Icon_pre from "../Image/icon/pre.png";
+import Icon_next from "../Image/icon/next.png";
 
-
-function Content1 () {
-    return (
-        <>
-            <div className="flex justify-between bg-black items-center absolute w-full">
-                <div className="flex ml-32 items-center gap-2">
-                    <img src="src/assets/Image/menu-4-16.png" alt="" width={25}/>
-                </div>
-                <div className="flex gap-2 mr-32">
-                    <button className="bg-[#ed642d]">Đăng nhập</button>
-                    <button className="bg-[#b7281d]">Đăng ký</button>
-                </div>
-            </div> 
-            <img src="https://img.gashinzo.com/img/ae888/ads/661f5077-df1a-4d19-a853-c5f7d975d525.jpg" alt="" />
-        
-        </>
-    )
+function SampleNextArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        right: "29px",
+        zIndex: 9,
+        backgroundImage: `url(${Icon_next})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        width: "70px",
+        height: "70px",
+        opacity: 0.6,
+      }}
+      onClick={onClick}
+    />
+  );
 }
-export default Content1
+
+function SamplePrevArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        left: "29px",
+        zIndex: 9,
+        backgroundImage: `url(${Icon_pre})`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        width: "70px",
+        height: "70px",
+        opacity: 0.6,
+      }}
+      onClick={onClick}
+    />
+  );
+}
+
+function Content1() {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
+  };
+  return (
+    <>
+      <div className="flex justify-between bg-black items-center absolute w-full">
+        <div className="flex ml-32 items-center gap-2">
+          <img src="src/assets/Image/menu-4-16.png" alt="" width={25} />
+        </div>
+        <div className="flex gap-2 mr-32">
+          <button className="bg-[#ed642d]">Đăng nhập</button>
+          <button className="bg-[#b7281d]">Đăng ký</button>
+        </div>
+      </div>
+      <Slider {...settings}>
+        <div>
+          <img
+            src="https://img.gashinzo.com/img/ae888/ads/661f5077-df1a-4d19-a853-c5f7d975d525.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="https://img.gashinzo.com/img/ae888/ads/661f5077-df1a-4d19-a853-c5f7d975d525.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          <img
+            src="https://img.gashinzo.com/img/ae888/ads/661f5077-df1a-4d19-a853-c5f7d975d525.jpg"
+            alt=""
+          />
+        </div>
+      </Slider>
+    </>
+  );
+}
+export default Content1;
--- src/assets/Component/Header.tsx
+++ src/assets/Component/Header.tsx
@@ -1,65 +1,242 @@
+import classes from "./styles.module.scss";
 
-
-function Header () {
-
-    return (
-        <>
-        <ul className="flex gap-6 justify-center mt-4">
-            <div>
-                <img src="https://img.gashinzo.com/img/static/desktop/brand/ae888/logo.png" alt="" width={900/4} height={244/4}/>
+function Header() {
+  return (
+    <div className="py-4 bg-white relative">
+      <div className="w-[1200px] m-auto flex gap-6 justify-center">
+        <div>
+          <img
+            src="https://img.gashinzo.com/img/static/desktop/brand/ae888/logo.png"
+            alt=""
+            width={900 / 4}
+            height={244 / 4}
+          />
+        </div>
+        <nav className={`flex gap-5 ${classes.nav}`}>
+          <ul>
+            <li>
+              <a href="#">
+                <img
+                  src="https://img.gashinzo.com/img/static/ae888/2023/navicon-sports.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                />
+                <span>THỂ THAO</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img
+                  src="https://img.gashinzo.com/img/static/ae888/2023/navicon-sports.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                />
+                <span>THỂ THAO</span>
+              </a>
+              <ul>
+                <li>
+                  <a href="#">
+                    <img
+                      src="https://img.gashinzo.com/img/static/desktop/sub-menu/sub-sports-saba.png"
+                      alt=""
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img
+                      src="https://img.gashinzo.com/img/static/desktop/sub-menu/sub-sports-saba.png"
+                      alt=""
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img
+                      src="https://img.gashinzo.com/img/static/desktop/sub-menu/sub-sports-saba.png"
+                      alt=""
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img
+                      src="https://img.gashinzo.com/img/static/desktop/sub-menu/sub-sports-saba.png"
+                      alt=""
+                    />
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <img
+                      src="https://img.gashinzo.com/img/static/desktop/sub-menu/sub-sports-saba.png"
+                      alt=""
+                    />
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li>
+              <a href="#">
+                <img
+                  src="https://img.gashinzo.com/img/static/ae888/2023/navicon-sports.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                />
+                <span>THỂ THAO</span>
+              </a>
+            </li>
+            <li>
+              <a href="#">
+                <img
+                  src="https://img.gashinzo.com/img/static/ae888/2023/navicon-sports.png"
+                  alt=""
+                  width={40}
+                  height={40}
+                />
+                <span>THỂ THAO</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      {/* <ul className="flex gap-6 justify-center mt-4">
+        <div>
+          <img
+            src="https://img.gashinzo.com/img/static/desktop/brand/ae888/logo.png"
+            alt=""
+            width={900 / 4}
+            height={244 / 4}
+          />
+        </div>
+        <div className="flex gap-5 ">
+          <div className={`${classes.dropdown}`}>
+            <button className={classes.dropbtn}>
+              <img
+                src="https://img.gashinzo.com/img/static/ae888/2023/navicon-sports.png"
+                alt=""
+                width={40}
+                height={40}
+              />
+              <p>THỂ THAO</p>
+            </button>
+            <div className={classes.dropdownContent}>
+              <a href="#">
+                <img src="https://img.gashinzo.com/img/static/desktop/sub-menu/sub-sports-saba.png" />
+              </a>
+              <a href="#">
+                <img src="https://img.gashinzo.com/img/static/desktop/sub-menu/sub-sports-saba.png" />
+              </a>
+              <a href="#">
+                <img src="https://img.gashinzo.com/img/static/desktop/sub-menu/sub-sports-saba.png" />
+              </a>
             </div>
-            <div className="flex gap-5 ">
-                <div className="flex flex-col items-center">
-                    <img src="https://img.gashinzo.com/img/static/ae888/2023/navicon-sports.png" alt="" width={40} height={40} />
-                    <li>THỂ THAO</li>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://img.gashinzo.com/img/static/ae888/2023/navicon-live.png" alt="" width={40} height={40}/>
-                    <li>SÒNG BÀI</li>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://img.gashinzo.com/img/static/ae888/2023/navicon-egame.png" alt="" width={40} height={40}/>
-                    <li>SLOTS</li>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://img.gashinzo.com/img/static/ae888/2023/navicon-cock.png" alt="" width={40} height={40}/>
-                    <li>ĐÁ GÀ</li>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://img.gashinzo.com/img/static/ae888/2023/navicon-chess.png" alt="" width={40} height={40}/>
-                    <li>GAME BÀI</li>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://img.gashinzo.com/img/static/ae888/2023/navicon-mpg.png" alt="" width={40} height={40}/>
-                    <li>BẮN CÁ</li>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://img.gashinzo.com/img/static/ae888/2023/navicon-lotto.png" alt="" width={40} height={40}/>
-                    <li>XỔ SỐ</li>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="	https://img.gashinzo.com/img/static/ae888/2023/navicon-esports.png" alt="" width={40} height={40}/>
-                    <li>E-SPORTS</li>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://img.gashinzo.com/img/static/ae888/2023/navicon-promo.png" alt="" width={40} height={40}/>
-                    <li>KHUYẾN MÃI</li>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://img.gashinzo.com/img/static/ae888/2023/navicon-vip.png" alt="" width={40} height={40}/>
-                    <li>VIP</li>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src="https://img.gashinzo.com/img/static/ae888/2023/navicon-daily.png" alt="" width={40} height={40}/>
-                    <li>ĐẠI LÝ NGOÀI</li>
-                </div>
-                <div>
-                    <img src="	https://img.gashinzo.com/img/static/ae888/2023/mobile/navicon-app.png" alt="" width={40} height={40}/>
-                    <li>APP</li>
-                </div>
-            </div>
-        </ul>
-        </>
-    )
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://img.gashinzo.com/img/static/ae888/2023/navicon-live.png"
+              alt=""
+              width={40}
+              height={40}
+            />
+            <li>SÒNG BÀI</li>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://img.gashinzo.com/img/static/ae888/2023/navicon-egame.png"
+              alt=""
+              width={40}
+              height={40}
+            />
+            <li>SLOTS</li>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://img.gashinzo.com/img/static/ae888/2023/navicon-cock.png"
+              alt=""
+              width={40}
+              height={40}
+            />
+            <li>ĐÁ GÀ</li>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://img.gashinzo.com/img/static/ae888/2023/navicon-chess.png"
+              alt=""
+              width={40}
+              height={40}
+            />
+            <li>GAME BÀI</li>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://img.gashinzo.com/img/static/ae888/2023/navicon-mpg.png"
+              alt=""
+              width={40}
+              height={40}
+            />
+            <li>BẮN CÁ</li>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://img.gashinzo.com/img/static/ae888/2023/navicon-lotto.png"
+              alt=""
+              width={40}
+              height={40}
+            />
+            <li>XỔ SỐ</li>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="	https://img.gashinzo.com/img/static/ae888/2023/navicon-esports.png"
+              alt=""
+              width={40}
+              height={40}
+            />
+            <li>E-SPORTS</li>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://img.gashinzo.com/img/static/ae888/2023/navicon-promo.png"
+              alt=""
+              width={40}
+              height={40}
+            />
+            <li>KHUYẾN MÃI</li>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://img.gashinzo.com/img/static/ae888/2023/navicon-vip.png"
+              alt=""
+              width={40}
+              height={40}
+            />
+            <li>VIP</li>
+          </div>
+          <div className="flex flex-col items-center">
+            <img
+              src="https://img.gashinzo.com/img/static/ae888/2023/navicon-daily.png"
+              alt=""
+              width={40}
+              height={40}
+            />
+            <li>ĐẠI LÝ NGOÀI</li>
+          </div>
+          <div>
+            <img
+              src="	https://img.gashinzo.com/img/static/ae888/2023/mobile/navicon-app.png"
+              alt=""
+              width={40}
+              height={40}
+            />
+            <li>APP</li>
+          </div>
+        </div>
+      </ul> */}
+    </div>
+  );
 }
-export default Header
+export default Header;
